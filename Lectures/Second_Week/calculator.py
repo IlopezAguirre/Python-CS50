2771783716b65aa4d992b024aa900711582d91c4
Ian Lopez <ian158699@gmail.com>
2026-05-11 23:10:47 -0400

    'Some work from Day 2 of studying, dataypes :int ...' with -a does not make sense

---
+++ Lectures/Second_Week/calculator.py
@@ -0,0 +1,17 @@
+#Concatenates string
+'''x =input("What's x? ")
+y = input("What's y? ")
+z = x+y
+print(z)'''
+
+'''x =input("What's x? ")
+y = input("What's y? ")
+z = int(x)+int(y) #int() is a function that takes a parameter like a string makes it an int
+print(z)'''
+
+#you can nest functions -- function inside another function
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+print(x + y)
+
+#float: is a floating point value- decimal in value
---
+++ Lectures/Second_Week/second_lecture.py
@@ -0,0 +1,1 @@
+#python has an interactive mode that allows us to run python inside of the terminal and completely execute code without compiling 
