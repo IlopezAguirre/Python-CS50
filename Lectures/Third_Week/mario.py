a62c652f6486d62b3a34f3a5f02a22a5b9bedad2
Ian Lopez <ian158699@gmail.com>
2026-06-10 20:31:32 -0400

    Finished 3 by 3 square example

--- Lectures/Third_Week/mario.py
+++ Lectures/Third_Week/mario.py
@@ -1,3 +1,20 @@
-print("#")
-print("#")
-print("#")
+def main():
+    #print_column(3)
+    #print_row(4):
+     print_square(3)   
+#def print_column(height):
+    #for _ in range(height):
+        #print("#")
+        
+#def print_row(width):
+    #print("?"* width)
+
+def print_square(size):
+    for i in range(size):
+        
+        #For each brick in row
+        for j in range(size):
+            #Print brick
+            print("#", end="")
+        print("#") 
+main()
