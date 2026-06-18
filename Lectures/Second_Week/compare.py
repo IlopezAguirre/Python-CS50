62164b68925ad8cdd34c8576d1f554ea2da13271
Ian Lopez <ian158699@gmail.com>
2026-06-17 22:08:42 -0400

    Add weight challenge implementation with planet weight calculations

--- Lectures/Second_Week/compare.py
+++ Lectures/Second_Week/compare.py
@@ -24,3 +24,31 @@ elif height >= 137 and credits < 10:
   print("You don't have enough credits.")
 else:
   ("You have not met the requirements")
+  
+  ###WEIGHT CHALLENGE ## IF & ELSES FLOW CONTROL 
+  # Write code below
+earth_weight = float(input("What's your weight on earth: "))
+planet_number = int(input("What's your planet number: "))
+if planet_number == 1:
+  destination_weight = earth_weight * 0.38
+  print(destination_weight)
+elif planet_number == 2:
+  destination_weight = earth_weight * 0.91
+  print(destination_weight)
+elif planet_number == 3:
+  destination_weight = earth_weight * 0.38
+  print(destination_weight)
+elif planet_number == 4:
+  destination_weight = earth_weight * 2.53
+  print(destination_weight)
+elif planet_number == 5:
+  destination_weight = earth_weight * 1.07
+  print(destination_weight)
+elif planet_number == 6:
+  destination_weight = earth_weight * 0.89
+  print(destination_weight)
+elif planet_number == 7:
+  destination_weight = earth_weight * 1.14
+  print(destination_weight)
+else:
+  print('Invalid number')
